0c91f61103a41a9d6885bd807d0c1f55f67f8a58
V-Pavan-Sastry <90331092+V-Pavan-Sastry@users.noreply.github.com>
2023-08-27 12:10:47 +0530

    Update server.py

--- server.py
+++ server.py
@@ -5,15 +5,21 @@ app = Flask(__name__)
 
 @app.route("/calculator/greeting", methods=['GET'])
 def greeting():
-    return ''
+    return 'Hello world!'
 
 @app.route("/calculator/add", methods=['POST'])
 def add():
-    return ''
+    first= request.form.get('first')
+    second= request.form.get('second')
+    res=first+second
+    return 'result: %',res
 
 @app.route("/calculator/subtract", methods=['POST'])
 def subtract():
-    return ''
+    first= request.form.get('first')
+    second= request.form.get('second')
+    res=first-second
+    return 'result: %',res
 
 if __name__ == '__main__':
     app.run(port=8080,host='0.0.0.0')
